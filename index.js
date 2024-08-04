45cd200c26e30475866927c9b449e7224fa63762
Asmaa-Saeed <asmaasaeed2082003@gmail.com>
2024-08-04 12:23:19 +0300

    upload quote generator website

---
+++ index.js
@@ -0,0 +1,36 @@
+// bring all the selectors which need logic
+let quoteIdEl = document.querySelector(".quote-id");
+let quoteDisplayEl = document.querySelector(".quote-display");
+let generateBtn = document.querySelector(".generate");
+let autoBtn = document.querySelector(".auto");
+let stopBtn = document.querySelector(".stop");
+let autoStatusEl = document.querySelector(".auto-status");
+
+// make the function that generates the code // here we use the set interval function to execute the code at regular intervals
+let intervalId;
+        generateBtn.addEventListener("click", generateQuote);
+        generateBtn.addEventListener("click", startAutoPlay);
+        stopBtn.addEventListener("click", stopAutoPlay);
+    async function getQuotes() {
+        const response = await fetch("quotes.json");
+        const data = await response.json();
+        return data;
+    };
+
+    async function generateQuote() {
+        const quotes = await getQuotes();
+        const quote = quotes[Math.floor(Math.random() * quotes.length)];
+        quoteDisplayEl.innerText = quote.text;
+        quoteIdEl.innerText= quote.id;
+    };
+
+    function startAutoPlay() {
+        intervalId = setInterval(generateQuote, 5000);
+        autoStatusEl.innerHTML = "Auto : ON";
+    };
+
+
+    function stopAutoPlay() {
+        clearInterval(intervalId);
+        autoStatusEl.innerHTML = " ";
+    }
